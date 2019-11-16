b92904b4d49f884376fe8737288a644a18db5635
ZItzak <43185740+ZItzak@users.noreply.github.com>
2019-11-16 22:42:50 +0100

    validate input removing extra newlines

--- app.py
+++ app.py
@@ -7,16 +7,17 @@ class App(tk.Frame, RoofPlates):
 
 	def __init__(self, the_window):
 		tk.Frame.__init__(self, the_window)
-		self.size_plank = tk.StringVar()
+		# self.user_input = tk.StringVar()
 		self.plates = RoofPlates()
 		test = [[110, 130], [130, 150], [150, 170], [170, 190], [190, 210]]
 		self.size_roof = dict.fromkeys((item[0] for item in test), False)
-		# self["config"] = 5
-		# self["width"] = 500
-		# self["bd"] = -10
 		self.display_string = tk.StringVar()
 		self.display_string = tk.StringVar()
-		self.format_string_example = "Input format verkeerd.\nGebruik alleen getallen met één streep(-) ertussen.\nDe getallen mogen alleen een verschil van 10 of 20\nhebben die bij alle maten gelijk moet zijn.\n\nVoorbeeld 1:\n170-190\n190-210\n210-230\n\nVoorbeeld 2:\n150-160\n160-170\n170-180"
+		self.format_string_example = "Input format verkeerd.\nGebruik alleen " \
+									"getallen met één streep(-) ertussen.\nDe getallen mogen" \
+									" alleen een verschil van 10 of 20\nhebben die bij alle maten gelijk moet zijn." \
+									"\n\nVoorbeeld 1:\n170-190\n190-210\n210-230\n\nVoorbeeld 2:\n150-160\n160-170\n" \
+									"170-180"
 
 		self.ask_size_label = tk.Label(self, text="Vul hier de maten in")
 		self.text_field = tk.Text(the_window, height=20, width=53)
@@ -28,54 +29,62 @@ class App(tk.Frame, RoofPlates):
 		self.button.grid(row=2, column=0)
 		self.display_label.grid(row=3, column=0)
 
-	
 	def validate_input(self):
 
 		user_input = self.text_field.get(1.0, "end")
 		user_input = re.split("[-\n]", user_input)
-		user_input.pop()
+		while True:
+			try:
+				user_input.remove('')
+			except:
+				break
+		print(user_input)
 		if all(x.isdigit() for x in user_input) is False:
 			self.display_output2(self.format_string_example)
-		else:
-			self.plates.make_vars(10, self.size_roof)
-			self.plates.run()
-			self.plates.print_test_final()
-		
-		
-		
 
+
+		# else:
+		#     # self.pl
+		#     self.plates.make_vars(20, self.size_roof)
+		#     self.plates.run()
+		#     self.plates.print_test_final()
 
 	def display_output2(self, strig):
 		self.text_field.delete(1.0, "end")
 		self.text_field.insert(1.0, strig)
 
+# 110-130
+# 130-150
+# 150-170
+# 170-190
+# 190-210
 
 # def input_to_backend(win):
 
-	# print("test")
-	# win.display_output2("3 x 100\n1 x 150 - 170\n1 x 170 - 190\n1 x 110 - 130")	
+# print("test")
+# win.display_output2("3 x 100\n1 x 150 - 170\n1 x 170 - 190\n1 x 110 - 130")
 # def display_output(win):
 # 	test = win.tekst.get(1.0, "end")
 # 	test = re.split("[-\n\0]", test)
 # 	test.pop()
 # 	if all(x.isdigit() for x in test) is False:
 # 		w
-	# test = [x for x in test if x.isdigit()]
-	
-	
-	# lambda win=self: display_output(win)
-	
-	# test.pop()
-	# if test is False
-	# 	print("False")
-	# for x in test:
-	# 	try:
-	# 		int(x)
-	# 	except:
-	# 		test.remove(x)
-	# test = int(test)
-	# print(test)
-	# win.display_output2("3 x 100\n1 x 150 - 170\n1 x 170 - 190\n1 x 110 - 130")
+# test = [x for x in test if x.isdigit()]
+
+
+# lambda win=self: display_output(win)
+
+# test.pop()
+# if test is False
+# 	print("False")
+# for x in test:
+# 	try:
+# 		int(x)
+# 	except:
+# 		test.remove(x)
+# test = int(test)
+# print(test)
+# win.display_output2("3 x 100\n1 x 150 - 170\n1 x 170 - 190\n1 x 110 - 130")
 
 # my_window = tk.Tk()
 # my_window.title("Dak platen omrekenen")
@@ -90,4 +99,3 @@ class App(tk.Frame, RoofPlates):
 # 150-170
 # 170-190
 # 210-230
-
--- roof_plate_size_converter.py
+++ roof_plate_size_converter.py
@@ -4,31 +4,31 @@ from app import App
 
 
 if __name__ == '__main__':
-	test = [[110, 130], [130, 150], [150, 170], [170, 190], [190, 210]]  ## <---------
-	# test = [[130, 150], [150, 170], [170, 190], [190, 210], [210, 230], [230, 250]]  ## <---------
-	# plates = RoofPlates()
-	# size_roof = dict.fromkeys((item[0] for item in test), False)
-	my_window = tk.Tk()
-	my_window.title("Dak platen omrekenen")
-	my_window.geometry("400x400")
-	frame_a = App(my_window)
-	frame_a.grid(row=0, column=0)
-	# print(frame_a.size_plank)
-	my_window.mainloop()
-
-
-	
-	# difference = abs(test[0][0] - test[0][1])
-	# if difference == 10:
-	# 	plates.make_vars(difference, size_roof)
-	# elif difference == 20:
-	# 	plates.make_vars(difference, size_roof)
-	# else:
-	# 	pass ##<-------- Invalid difference
+    test = [[110, 130], [130, 150], [150, 170], [170, 190], [190, 210]]  ## <---------
+    # test = [[130, 150], [150, 170], [170, 190], [190, 210], [210, 230], [230, 250]]  ## <---------
+    # plates = RoofPlates()
+    # size_roof = dict.fromkeys((item[0] for item in test), False)
+    my_window = tk.Tk()
+    my_window.title("Dak platen omrekenen")
+    my_window.geometry("450x500")
+    frame_a = App(my_window)
+    frame_a.grid(row=0, column=0)
+    # print(frame_a.size_plank)
+    my_window.mainloop()
 
 
 
-	# plates.run()
+    # difference = abs(test[0][0] - test[0][1])
+    # if difference == 10:
+    # 	plates.make_vars(difference, size_roof)
+    # elif difference == 20:
+    # 	plates.make_vars(difference, size_roof)
+    # else:
+    # 	pass ##<-------- Invalid difference
 
-	# print("FINAL")
-	# plates.print_test_final()
+
+
+    # plates.run()
+
+    # print("FINAL")
+    # plates.print_test_final()
